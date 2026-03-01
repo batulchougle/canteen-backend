d34c2055ecac032c9576638385cd868b655c410a
batulchougle <batulchougle@gmail.com>
2026-03-01 22:38:14 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,14 +4,3 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-```
-
-**`Procfile`** (create this in root):
-```
-web: gunicorn canteen_system.wsgi:application
-```
-
-Make sure `gunicorn` and `whitenoise` are in your `requirements.txt`:
-```
-gunicorn
-whitenoise

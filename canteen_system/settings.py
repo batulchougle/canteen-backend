1cb5c2a9428f2b4793dcb78e3820d47a369cf100
batulchougle <batulchougle@gmail.com>
2026-03-02 00:41:31 +0530

    Update settings.py

--- canteen_system/settings.py
+++ canteen_system/settings.py
@@ -39,7 +39,6 @@ DEBUG = False
 
 ALLOWED_HOSTS = [ '*']
 
-DEBUG = True
 EMAIL_HOST ='smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')

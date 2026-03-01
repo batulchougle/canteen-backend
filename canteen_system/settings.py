8374483fb21cf1a6c0307549957d8a5b0bce1044
batulchougle <batulchougle@gmail.com>
2026-03-01 23:55:43 +0530

    Update settings.py

--- canteen_system/settings.py
+++ canteen_system/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -100,12 +101,10 @@ AUTH_USER_MODEL="users.User"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+       'default': dj_database_url.config(
+           default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3')
+       )
+   }
 REST_FRAMEWORK={
     
       'NON_FIELD_ERRORS_KEY':'error',

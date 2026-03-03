d1b2599d97aa3867124e0cad4b218d94a5f3329f
batulchougle <batulchougle@gmail.com>
2026-03-03 15:33:26 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-echo "from django.contrib.auth import get_user_model; U = get_user_model(); U.objects.filter(is_superuser=True).exists() or U.objects.create_superuser('Admin', 'admin', 'admin123')" | python manage.py shell
+echo "from users.models import User; User.objects.filter(is_superuser=True).exists() or User.objects.create_superuser('Admin', 'admin', 'admin123', role='admin', is_email_verified=True)" | python manage.py shell

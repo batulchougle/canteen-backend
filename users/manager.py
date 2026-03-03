8bb34986eb7c4559bfe52ce701890ba8afe0c904
batulchougle <batulchougle@gmail.com>
2026-03-03 15:32:11 +0530

    Update manager.py

--- users/manager.py
+++ users/manager.py
@@ -25,7 +25,7 @@ class UserManager(BaseUserManager):
     def create_superuser(self,name,username,password,**extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
-        extra_fields.setdefault("is_verified", True)
+        extra_fields.setdefault("is_email_verified", True)
 
         if extra_fields.get("is_staff") is not True:
             raise ValueError(_("is staff must be true for admin user"))

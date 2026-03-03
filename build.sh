ddfbd54724ad308380f9ee316914a2c7a859f19b
batulchougle <batulchougle@gmail.com>
2026-03-03 15:44:27 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,3 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-echo "from users.models import User; User.objects.filter(is_superuser=True).exists() or User.objects.create_superuser('Admin', 'admin', 'admin123', role='admin', is_email_verified=True)" | python manage.py shell

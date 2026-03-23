250b6618e297282a3ece9533f9624069c41973c9
batulchougle <batulchougle@gmail.com>
2026-03-23 11:21:54 +0530

    Update utility.py

--- users/utility.py
+++ users/utility.py
@@ -1,23 +1,24 @@
 import random
 import threading
 from typing import Optional
+from django.core.mail import EmailMessage
 from django.conf import settings
-import resend
 from .models import User, OneTimePassword
-import os
 
 def generate_otp(length: int = 6) -> str:
     return ''.join(str(random.randint(0, 9)) for _ in range(length))
 
 def _send_email(name, to_email, otp_code):
     try:
-        resend.api_key = os.environ.get('RESEND_API_KEY')
-        resend.Emails.send({
-            "from": "onboarding@resend.dev",
-            "to": to_email,
-            "subject": "One-Time Passcode for Email Verification",
-            "text": f"Dear {name},\n\nYour OTP is: {otp_code}\n\nDo not share this with anyone."
-        })
+        subject = "One-Time Passcode for Email Verification"
+        body = (
+            f"Dear {name},\n\n"
+            f"Your OTP is: {otp_code}\n\n"
+            f"Do not share this with anyone.\n\n"
+            f"Best regards,\nCafe Queue Team"
+        )
+        message = EmailMessage(subject=subject, body=body, from_email=settings.DEFAULT_FROM_EMAIL, to=[to_email])
+        message.send(fail_silently=False)
         print(f"EMAIL SENT to {to_email}")
     except Exception as e:
         print(f"EMAIL ERROR: {e}")
